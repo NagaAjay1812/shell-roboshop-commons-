8d4e4377ccb8547364134d9586cc318f6e481263
Naga Ajay <nagaajay@mac.mynetworksettings.com>
2026-02-06 15:41:17 -0500

    MongoDB Installation

---
+++ mongodb.sh
@@ -0,0 +1,17 @@
+source ./commons.sh
+ROOT_ACESS
+START_TIMER
+
+cp mongo.repo /etc/yum.repos.d/
+VALIDATE $? "Copying mongo repo"
+APP_MODULE=mongod
+MONGODB_SETUP
+
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> "$LOGS_FILE"
+VALIDATE $? "Allowing the remote connection"
+
+
+SERVICE_SETUP
+SERVICE_RESTART_SETUP
+END_TIMER
